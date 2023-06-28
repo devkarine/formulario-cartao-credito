fe0c602e2840d65657d0f5df8e3b0dbac322df61
Karine Pereira <karinedeveloper@gmail.com>
2023-06-28 15:28:23 -0300

    incluso variaveis dos inputs e cartao de credito

--- src/js/script.js
+++ src/js/script.js
@@ -1,21 +1,31 @@
-const input = document.querySelectorAll("input");
+const numberCard = document.getElementById("card-number");
+const nameClient = document.getElementById("card-name");
+const dataVenc = document.getElementById("card-expiration");
+const numberCvc = document.getElementById("card-cvc");
+const name = document.getElementById("name");
+const number = document.getElementById("number");
+const month = document.getElementById("month");
+const year = document.getElementById("year");
+const cvc = document.getElementById("cvc");
 const btnConfirm = document.getElementById("btn-confirm");
 const error = document.querySelectorAll("small");
 
 btnConfirm.addEventListener("click", (event) => {
   event.preventDefault();
 
-  input.forEach((element,index) => {
-     const elementValue = element.value
-    if(elementValue === ""){
-        error[index].classList.add("error");
-        error[index].innerText = "Can't be blank";
-    }else{
-        error[index].classList.remove("error");
-        error[index].innerText = ""
-       
+  const inputFields = [name, number, month, year, cvc];
+  inputFields.forEach((element, index) => {
+    const elementValue = element.value;
+    if (elementValue === "") {
+      error[index].classList.add("error");
+      error[index].innerText = "Can't be blank";
+      element.style.border = "1px solid red";
+    } else {
+      error[index].classList.remove("error");
+      error[index].innerText = "";
+      element.style.border = "";
     }
   });
-
-  
 });
+
+
