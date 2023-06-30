bcff586c108831d740e5052a2792552fd003b59a
Karine Pereira <karinedeveloper@gmail.com>
2023-06-29 22:03:37 -0300

    implentado validacoes no js

--- src/js/script.js
+++ src/js/script.js
@@ -4,37 +4,90 @@ const dataVenc = document.getElementById("card-expiration");
 const numberCvc = document.getElementById("card-cvc");
 const nameClient = document.getElementById("name");
 const numberClient = document.getElementById("number");
-
 const monthVenc = document.getElementById("month");
 const yearVenc = document.getElementById("year");
-
 const cvc = document.getElementById("cvc");
-
 const btnConfirm = document.getElementById("btn-confirm");
 const btnContinue = document.getElementById("btn-continue");
 const error = document.querySelectorAll("small");
 const input = document.querySelectorAll("input");
-const form = document.querySelector(".form");
-const thanks = document.querySelector(".thanks");
+const form = document.getElementById("form");
+const thanks = document.getElementById("thanks");
 
-btnConfirm.addEventListener("click", (event) => {
+function validateName() {
+  const name = nameClient.value.trim();
+  if (name === "") {
+    error[0].classList.add("error");
+    error[0].innerText = "Can't be blank";
+    nameClient.style.border = "1px solid red";
+    return false;
+  }
+  error[0].classList.remove("error");
+  error[0].innerText = "";
+  nameClient.style.border = "";
+  return true;
+}
+
+function validateCardNumber() {
+  const cardNumber = numberClient.value.trim().replace(/\s/g, "");
+  if (cardNumber === "") {
+    error[1].classList.add("error");
+    error[1].innerText = "Can't be blank";
+    numberClient.style.border = "1px solid red";
+    return false;
+  }
+  error[1].classList.remove("error");
+  error[1].innerText = "";
+  numberClient.style.border = "";
+  return true;
+}
+
+function validateExpirationDate() {
+  const month = monthVenc.value.trim();
+  const year = yearVenc.value.trim();
+  if (month === "" || year === "") {
+    error[2].classList.add("error");
+    error[2].innerText = "Can't be blank";
+    monthVenc.style.border = "1px solid red";
+    yearVenc.style.border = "1px solid red";
+    return false;
+  }
+  error[2].classList.remove("error");
+  error[2].innerText = "";
+  monthVenc.style.border = "";
+  yearVenc.style.border = "";
+  return true;
+}
+
+function validateCVC() {
+  const cvcValue = cvc.value.trim();
+  if (cvcValue === "" || cvcValue.length !== 3 || !/^\d+$/.test(cvcValue)) {
+    error[3].classList.add("error");
+    error[3].innerText = "Invalid CVC";
+    cvc.style.border = "1px solid red";
+    return false;
+  }
+  error[3].classList.remove("error");
+  error[3].innerText = "";
+  cvc.style.border = "";
+  return true;
+}
+
+function validateForm(event) {
   event.preventDefault();
 
-  input.forEach((element, index) => {
-    const elementValue = element.value;
-    if (elementValue === "") {
-      error[index].classList.add("error");
-      error[index].innerText = "Can't be blank";
-      element.style.border = "1px solid red";
-    } else {
-      error[index].classList.remove("error");
-      error[index].innerText = "";
-      element.style.border = "";
-      thanks.classList.remove("visibility");
-      form.classList.add("visibility");
-    }
-  });
-});
+  const isValidName = validateName();
+  const isValidCardNumber = validateCardNumber();
+  const isValidExpirationDate = validateExpirationDate();
+  const isValidCVC = validateCVC();
+
+  if (isValidName && isValidCardNumber && isValidExpirationDate && isValidCVC) {
+    thanks.classList.remove("visibility");
+    form.classList.add("visibility");
+  }
+}
+
+btnConfirm.addEventListener("click", validateForm);
 
 btnContinue.addEventListener("click", (event) => {
   event.preventDefault();
@@ -50,15 +103,21 @@ nameClient.addEventListener("input", () => {
 numberClient.addEventListener("input", () => {
   const numberClientValue = numberClient.value;
   numberCard.innerText = numberClientValue;
+  limitCardNumber();
 });
 
-monthVenc.addEventListener("input", insertDataVenc);
-yearVenc.addEventListener("input", insertDataVenc);
+function formatCardNumber(value) {
+  const formattedValue = value.replace(/\s/g, "").replace(/(\d{4})/g, "$1 ");
+  return formattedValue.trim();
+}
 
-cvc.addEventListener("input", () => {
-  const cvcValue = cvc.value;
-  numberCvc.innerText = cvcValue;
-});
+function limitCardNumber() {
+  const maxDigits = 16;
+  let formattedValue = numberClient.value.replace(/\D/g, "").slice(0, maxDigits);
+  const formattedNumber = formatCardNumber(formattedValue);
+  numberClient.value = formattedNumber;
+  numberCard.textContent = formattedNumber !== "" ? formattedNumber : "0000 0000 0000 0000";
+}
 
 function insertDataVenc() {
   const yearVencValue = yearVenc.value;
@@ -66,62 +125,48 @@ function insertDataVenc() {
   dataVenc.innerText = `${monthVencValue}/${yearVencValue}`;
 }
 
-numberClient.addEventListener("keydown", (event) => {
-  const codigoTecla = event.keyCde || event.which;
-  if (
-    !(
-      (codigoTecla >= 48 && codigoTecla <= 57) ||
-      codigoTecla == 8 ||
-      codigoTecla == 9 ||
-      codigoTecla == 37 ||
-      codigoTecla == 39
-    )
-  ) {
+function handleNumericInput(event) {
+  const allowedKeys = ["Backspace", "Tab", "ArrowLeft", "ArrowRight"];
+  if (!/^\d$/.test(event.key) && !allowedKeys.includes(event.key)) {
     event.preventDefault();
+  }
+}
+
+function handleMonthInput(event) {
+  const maxDigits = 2;
+  const monthValue = monthVenc.value.replace(/\D/g, "").slice(0, maxDigits);
+  monthVenc.value = monthValue;
+}
+
+function handleYearInput(event) {
+  const maxDigits = 2;
+  const yearValue = yearVenc.value.replace(/\D/g, "").slice(0, maxDigits);
+  yearVenc.value = yearValue;
+}
+
+nameClient.addEventListener("keypress", (e) => {
+  if (/^\d$/.test(e.key)) {
+    e.preventDefault();
   }
 });
 
-monthVenc.addEventListener("keydown", (event) => {
-  const codigoTecla = event.keyCde || event.which;
-  if (
-    !(
-      (codigoTecla >= 48 && codigoTecla <= 57) ||
-      codigoTecla == 8 ||
-      codigoTecla == 9 ||
-      codigoTecla == 37 ||
-      codigoTecla == 39
-    )
-  ) {
-    event.preventDefault();
-  }
+[numberClient, monthVenc, yearVenc].forEach((input) => {
+  input.addEventListener("keydown", handleNumericInput);
 });
 
-yearVenc.addEventListener("keydown", (event) => {
-  const codigoTecla = event.keyCde || event.which;
-  if (
-    !(
-      (codigoTecla >= 48 && codigoTecla <= 57) ||
-      codigoTecla == 8 ||
-      codigoTecla == 9 ||
-      codigoTecla == 37 ||
-      codigoTecla == 39
-    )
-  ) {
-    event.preventDefault();
-  }
+monthVenc.addEventListener("input", insertDataVenc);
+yearVenc.addEventListener("input", insertDataVenc);
+monthVenc.addEventListener("input", handleMonthInput);
+yearVenc.addEventListener("input", handleYearInput);
+
+cvc.addEventListener("input", () => {
+  const cvcValue = cvc.value;
+  numberCvc.innerText = cvcValue.slice(0, 3); 
 });
 
-cvc.addEventListener("keydown", (event) => {
-  const codigoTecla = event.keyCde || event.which;
-  if (
-    !(
-      (codigoTecla >= 48 && codigoTecla <= 57) ||
-      codigoTecla == 8 ||
-      codigoTecla == 9 ||
-      codigoTecla == 37 ||
-      codigoTecla == 39
-    )
-  ) {
-    event.preventDefault();
+cvc.addEventListener("input", () => {
+  const cvcValue = cvc.value;
+  if (cvcValue.length > 3) {
+    cvc.value = cvcValue.slice(0, 3);
   }
 });
